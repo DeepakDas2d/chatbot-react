85df469de2e192070a0d62f1e6505b24c53a19c7
deepakdas2d <contacttodeepakdas@gmail.com>
2025-01-28 21:59:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useRef, useState } from "react";
+import ChatbotIcon from "./components/ChatbotIcon";
+import ChatForm from "./components/ChatForm";
+import ChatMessage from "./components/ChatMessage";
+
+const App = () => {
+  const [chatHistory, setChatHistory] = useState([]);
+  const chatBodyRef = useRef();
+
+  const generateBotResponse = async (history) => {
+    // Helper function to update chat history
+    const updateHistory = (text) => {
+      setChatHistory((prev) => [
+        ...prev.filter((msg) => msg.text !== "..."),
+        { role: "model", text },
+      ]);
+    };
+    // Format chat history for API request
+    history = history.map(({ role, text }) => ({ role, parts: [{ text }] }));
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ contents: history }),
+    };
+
+    try {
+      // make the API call to get the bot's response
+      const response = await fetch(
+        import.meta.env.VITE_API_URL,
+        requestOptions
+      );
+      const data = await response.json();
+      if (!response.ok)
+        throw new Error(data.error.message || "Something went wrong");
+
+      // Clean and update chat history with bot's response
+      const apiResponseText = data.candidates[0].content.parts[0].text
+        .replace(/\*\*(.*?)\*\*/g, "$1")
+        .trim();
+      updateHistory(apiResponseText);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    // Auto-scroll whenever chat history updates
+    chatBodyRef.current.scrollTo({
+      top: chatBodyRef.current.scrollHeight,
+      behavior: "smooth",
+    });
+  }, [chatHistory]);
+  return (
+    <div className="container">
+      <div className="chatbot-popup">
+        {/* chatbot header  */}
+        <div className="chat-header">
+          <div className="header-info">
+            <ChatbotIcon />
+            <h2 className="logo-text">Chatbot</h2>
+          </div>
+          <button className="material-symbols-rounded">
+            keyboard_arrow_down
+          </button>
+        </div>
+
+        {/* chatbot-body  */}
+
+        <div className="chat-body" ref={chatBodyRef}>
+          {/* Render the chat history dynamically  */}
+          {chatHistory.map((chat, index) => (
+            <ChatMessage key={index} chat={chat} />
+          ))}
+        </div>
+
+        {/* chatbot footer  */}
+        <div className="chat-footer">
+          <ChatForm
+            setChatHistory={setChatHistory}
+            generateBotResponse={generateBotResponse}
+            chatHistory={chatHistory}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ChatbotIcon.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChatbotIcon = () => {
+  return <i className="fas fa-robot"></i>;
+};
+
+export default ChatbotIcon;
